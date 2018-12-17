742970dc13cbcf6196a46b3915268855bea03882
Josef Kolář <thejoeejoee@gmail.com>
2018-12-17 14:13:37 +0100

    FisNode - used class instead of function

--- src/fis-node/node.js
+++ src/fis-node/node.js
@@ -1,16 +1,18 @@
-module.exports = function (RED) {
-    function FisNode(config) {
-        RED.nodes.createNode(this, config);
-        this.broker = RED.nodes.getNode(config.broker);
-        this.topic = ['', 'node', config.nodeId].join('/');
-    }
+module.exports = RED => {
+    class FisNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            this.broker = RED.nodes.getNode(config.broker);
+            this.topic = ['', 'node', config.nodeId].join('/');
+        }
 
-    FisNode.prototype.publish = function (payload) {
-        this.broker.publish({
-            topic: this.topic,
-            payload: payload,
-        })
-    };
+        publish(payload) {
+            this.broker.publish({
+                topic: this.topic,
+                payload: payload,
+            })
+        };
+    }
 
     RED.nodes.registerType("fis-node", FisNode);
 };

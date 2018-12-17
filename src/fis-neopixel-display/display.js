6302e123cda2b5a75abdbd28ef51af0e3df6f2c7
Josef Kolář <thejoeejoee@gmail.com>
2018-12-17 18:33:05 +0100

    Support for config node && display settings

--- src/fis-neopixel-display/display.js
+++ src/fis-neopixel-display/display.js
@@ -1,8 +1,20 @@
 module.exports = function (RED) {
+    const APP = 'neopixel-display';
+
     class NeoPixelDisplay {
+
         constructor(config) {
             RED.nodes.createNode(this, config);
+            /**
+             * @type FisNode
+             */
             this.fisNode = RED.nodes.getNode(config.node);
+
+            this.fisNode.config(APP, this.id, {
+                port: config.displayPort,
+                width: config.width,
+                height: config.height,
+            });
 
             this.on('input', msg => {
                 let payload = {
@@ -12,14 +24,11 @@ module.exports = function (RED) {
                 if (msg.color)
                     payload.color = msg.color;
 
-                this.fisNode.publish({
-                    app: 'neopixel-display',
-                    payload,
-                })
-
+                this.fisNode.publish(this.id, payload);
             });
         }
     }
+
 
     RED.nodes.registerType("fis-neopixel-display", NeoPixelDisplay);
 };
--- src/fis-node/node.js
+++ src/fis-node/node.js
@@ -2,15 +2,56 @@ module.exports = RED => {
     class FisNode {
         constructor(config) {
             RED.nodes.createNode(this, config);
+            /**
+             * @type MQTTBrokerNode
+             */
             this.broker = RED.nodes.getNode(config.broker);
             this.topic = ['', 'node', config.nodeId].join('/');
+            this.broker.connect();
+            this.config('config', 'config', {id: this.id}); // TODO: fuu?
+
+            this.on('close', (removed, done) => {
+                if (removed) {
+                    // TODO: call config and call remove from node
+                } else {
+                    // TODO: something on restart?
+                }
+                done();
+            });
         }
 
-        publish(payload) {
-            this.broker.publish({
+        publish(appId, payload) {
+            return this._publish({
+                app_id: appId,
+                payload,
+            });
+
+        };
+
+        config(app, app_id, config) {
+            console.log('CONFIG', app, app_id, config);
+            this.publish(
+                'config',
+                {
+                    app,
+                    config,
+                    app_id,
+                }
+            )
+        };
+
+        _publish(payload) {
+            console.log('PUBLISH', this.topic, payload);
+            const publish = () => this.broker.publish({
                 topic: this.topic,
-                payload: payload,
-            })
+                payload,
+            });
+
+            if (this.broker.connected)
+                publish();
+            else
+                this.broker.client.on('connect', publish)
+
         };
     }
 

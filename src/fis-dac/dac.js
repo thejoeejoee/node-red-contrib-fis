9a85f323cae1af3b457977cfcb2e8731fc7468fa
Josef Kolář <thejoeejoee@gmail.com>
2019-01-29 00:37:21 +0100

    DAC app implementation

---
+++ src/fis-dac/dac.js
@@ -0,0 +1,34 @@
+module.exports = function (RED) {
+    const APP = 'dac';
+
+    class DAC {
+
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            /**
+             * @type FisNode
+             */
+            this.fisNode = RED.nodes.getNode(config.node);
+
+            if (!this.fisNode) return;
+
+            this.fisNode.status_cb = this.status.bind(this);
+
+            this.fisNode.config(APP, this.id, {
+                port: config.dacPort,
+            });
+
+            this.on('input', msg => {
+                let payload = {
+                    value: msg.payload
+                };
+
+                payload.retain = true;
+                this.fisNode.appPublish(this.id, payload, 'value');
+            });
+        }
+    }
+
+
+    RED.nodes.registerType("fis-dac", DAC);
+};
